7e47de80aa7d362fa66b3ba22693f0d31ed7dd57
RgnDunes <singh.divyansh1802@gmail.com>
2020-11-22 22:34:31 +0530

    Improved Profile Page, Added Profile Picture Functionality

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
-    username=StringField('Username',validators=[DataRequired(), Length(min=2,max=10)])
+    username=StringField('Username',validators=[DataRequired(), Length(min=2,max=20)])
     email = StringField('Email',validators=[DataRequired(),Email()])
     password=PasswordField('Password',validators=[DataRequired(),Length(min=5, max=10)])
     confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),Length(min=5, max=10), EqualTo('password')])
@@ -22,6 +24,25 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired(),Length(min=5, max=10)])
+    password=PasswordField('Password',validators=[DataRequired(),Length(min=5, max=20)])
     remember=BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired(), Length(min=2,max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png','jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data!=current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username is already taken.')
+
+    def validate_email(self, email):
+        if email.data!=current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already exists.')
+
